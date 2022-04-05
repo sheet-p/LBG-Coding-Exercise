39e230790aadac631f5df5648b861e27b03431b8
sheet <sheetal.mec@gmail.com>
2022-04-05 01:53:51 +0100

    implementation: added parameters

--- springbootDemo/src/main/java/com/lbg/springbootDemo/controller/RestSpringBootController.java
+++ springbootDemo/src/main/java/com/lbg/springbootDemo/controller/RestSpringBootController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,12 +16,12 @@ public class RestSpringBootController {
     Logger logger = LoggerFactory.getLogger(RestSpringBootController.class);
 
     @GetMapping(value = "/atmInfo")
-    public ResponseEntity<?> getATM() {
+    public ResponseEntity<?> getATM(@RequestParam(name = "url")String link, @RequestParam(name = "identification")String identifier) {
         logger.trace("ATM method accessed");
          
-        String url = "https://api.lloydsbank.com/open-banking/v2.2/atms";
+        //String url = "https://api.lloydsbank.com/open-banking/v2.2/atms";
         RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.getForObject(url, String.class);
+        String response = restTemplate.getForObject(link, String.class);
 
         JSONObject result = new JSONObject(response);
         JSONArray data = result.getJSONArray("data");
@@ -28,6 +29,19 @@ public class RestSpringBootController {
         JSONObject jobj = brands.getJSONObject(0);
         JSONArray atms = jobj.getJSONArray("ATM");
 
-        return new ResponseEntity<>(atms.toString(), HttpStatus.OK);
+        //to store final list of matching ATMs
+        StringBuilder matchATMs =new StringBuilder();
+
+        //loop through arrays to find the matching identification
+        for(int loop=0; loop<atms.length(); loop++) {
+            JSONObject each = atms.getJSONObject(loop);
+
+            String ident = each.getString("Identification");
+            if(ident.equals(identifier)) {
+                matchATMs.append(each.toString());
+            }
+        }
+
+        return new ResponseEntity<>(matchATMs.toString(), HttpStatus.OK);
     }
 }
